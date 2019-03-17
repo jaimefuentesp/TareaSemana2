8f8e6471d265515ec059ecd71cec6cc91a6adfe4
jaimefuentesp <jaimefuentesp@outlook.com>
2019-03-16 20:25:39 -0500

    Final

--- app/src/main/java/com/jaimefuentesp/tareasemana2/Confimacion.java
+++ app/src/main/java/com/jaimefuentesp/tareasemana2/Confimacion.java
@@ -1,13 +1,56 @@
 package com.jaimefuentesp.tareasemana2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class Confimacion extends AppCompatActivity {
+
+    private TextView tvNombre;
+    private TextView tvTelefono;
+    private TextView tvMail;
+    private TextView tvDesripcion;
+    private TextView tvFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confimacion);
+
+        Bundle parametros = getIntent().getExtras();
+
+        String nombre=parametros.getString(getResources().getString(R.string.nombre));
+        String telefono=parametros.getString(getResources().getString(R.string.telefono));
+        String email=parametros.getString(getResources().getString(R.string.mail));
+        String descripcion=parametros.getString(getResources().getString(R.string.descripcion));
+        String fecha=parametros.getString(getResources().getString(R.string.fechaNac));
+
+        tvNombre =(TextView) findViewById(R.id.lnlConfirNombre);
+        tvTelefono =(TextView) findViewById(R.id.lnlConfirTelefono);
+        tvMail =(TextView) findViewById(R.id.lnlConfirMail);
+        tvDesripcion =(TextView) findViewById(R.id.lnlConfirDesc);
+        tvFecha =(TextView) findViewById(R.id.lnlConfirFecha);
+
+        tvNombre.setText(nombre);
+        tvTelefono.setText(telefono);
+        tvMail.setText(email);
+        tvDesripcion.setText(descripcion);
+        tvFecha.setText(fecha);
+
+        Button botonEditar = findViewById(R.id.botonEditar);
+        botonEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                //Toast.makeText(getBaseContext(),textoNombre.getText().toString(), Toast.LENGTH_SHORT).show();
+                //Intent intentMain =new Intent(Confimacion.this,MainActivity.class);
+                //startActivity(intentMain);
+            }
+        });
     }
 }
--- app/src/main/java/com/jaimefuentesp/tareasemana2/MainActivity.java
+++ app/src/main/java/com/jaimefuentesp/tareasemana2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.jaimefuentesp.tareasemana2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botonSiguiente = findViewById(R.id.botonSiguiente);
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textoNombre= (EditText) findViewById(R.id.txtInputNombre);
+                EditText textoTelef= (EditText) findViewById(R.id.txtInputTelefono);
+                EditText textoMail= (EditText) findViewById(R.id.txtInputEmail);
+                EditText textoDesc= (EditText) findViewById(R.id.txtInputDescContacto);
+                DatePicker dpFecha = (DatePicker) findViewById(R.id.dpFecha);
+                String fecha;
+                //Toast.makeText(getBaseContext(),textoNombre.getText().toString(), Toast.LENGTH_SHORT).show
+                Intent intentConfirmacion =new Intent(MainActivity.this,Confimacion.class);
+                intentConfirmacion.putExtra(getResources().getString(R.string.nombre),textoNombre.getText().toString());
+                intentConfirmacion.putExtra(getResources().getString(R.string.telefono),textoTelef.getText().toString());
+                intentConfirmacion.putExtra(getResources().getString(R.string.mail),textoMail.getText().toString());
+                intentConfirmacion.putExtra(getResources().getString(R.string.descripcion),textoDesc.getText().toString());
+                fecha = Integer.toString(dpFecha.getDayOfMonth()) +"/"+ Integer.toString(dpFecha.getMonth()+1)+"/"+Integer.toString(dpFecha.getYear());
+                intentConfirmacion.putExtra(getResources().getString(R.string.fechaNac),fecha);
+                startActivity(intentConfirmacion);
+                //finish()
+            }
+        });
     }
 }
